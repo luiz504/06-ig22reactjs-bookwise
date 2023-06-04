d2a8246d3b91d24950af3094e061d89f41635953
Luiz <l.bueno@xsensors.ai>
2023-06-04 14:36:21 -0300

    chore(config): stitches config and styled guide integration

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,26 @@
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import { globalStyles } from '~/styles/global'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'pt_BR',
+          url: process.env.APP_DOMAIN,
+          siteName: 'Ignite Call',
+        }}
+      />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,41 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from './tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    width: 'space',
+    minWidth: 'space',
+    maxWidth: 'space',
+    height: 'space',
+    minHeight: 'space',
+    maxHeight: 'space',
+  },
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+  prefix: 'bw',
+})
